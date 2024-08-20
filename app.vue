4c1298aa71857d6e33fa894c8be9a9ebd8ffbefd
Kylian, Jakub <jacob.kylian@gmail.com>
2024-08-20 17:22:08 +0200

    init

---
+++ app.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+useHead({
+  title: '3D App',
+  script: [
+    { src: 'https://ajax.googleapis.com/ajax/libs/model-viewer/3.5.0/model-viewer.min.js', type: 'module' },
+  ]
+})
+</script>
+
+<template>
+  <div class="container">
+
+    <model-viewer
+      alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum"
+      src="content.glb" ar shadow-intensity="1" camera-controls touch-action="pan-y"
+      style="width: 100%; height: 100%;" />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 90vw;
+  height: 90vh;
+}
+</style>
